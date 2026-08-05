836cfae78b82af22069f8170da11cb85491fe07e
Robotics-Team-6510 <157866421+Robotics-Team-6510@users.noreply.github.com>
2026-08-05 17:32:44 +1000

    Added BuiltDifferent Code

---
+++ src/main/java/org/firstinspires/ftc/teamcode/NEW_BuiltDifferent_Teleop.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BuiltDifferent")
+public class NEW_BuiltDifferent_Teleop extends OpMode {
+
+    //Declare OpMode members.
+ 
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
