7796b9f8614fd8b8bb9955e27e9008f16ac6f83d
Robotics-Team-6510 <RTeam@pymblelc.nsw.edu.au>
2026-08-06 18:23:31 +1000

    Botzilla Code

--- src/main/java/org/firstinspires/ftc/teamcode/NEW_botzilla_challenge1.java
+++ src/main/java/org/firstinspires/ftc/teamcode/NEW_botzilla_challenge1.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,7 +9,7 @@ import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-@TeleOp(name = "i make understanding :)")
+@TeleOp
 public class NEW_botzilla_challenge1 extends OpMode {
     // 1 - varibale definition
 
@@ -20,8 +22,6 @@ public class NEW_botzilla_challenge1 extends OpMode {
 
     IMU imu;
 
-
-
     @Override
     public void init(){
         // 2
@@ -33,17 +33,14 @@ public class NEW_botzilla_challenge1 extends OpMode {
         imu = hardwareMap.get(IMU.class, "imu");
 
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
-                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
         imu.initialize(parameters);
 
-        FR.setDirection(DcMotorSimple.Direction.FORWARD);
-        BR.setDirection(DcMotorSimple.Direction.FORWARD);
-        FL.setDirection(DcMotorSimple.Direction.REVERSE);
-        BL.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
-
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
 
     }
 
@@ -51,19 +48,39 @@ public class NEW_botzilla_challenge1 extends OpMode {
     @Override
     public void loop(){
         // 3
-        double forward = gamepad1.left_stick_y;
-        double sideways = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
         double turning = gamepad1.right_stick_x;
-                    // number
-        FR.setPower(forward - turning - sideways);
-        FL.setPower(forward + turning + sideways);
-        BR.setPower(forward - turning + sideways);
-        BL.setPower(forward + turning - sideways);
+
+        if(gamepad1.b){
+            imu.resetYaw();
+        }
+
+        double angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double forward = x * Math.sin(-angle) + y * Math.cos(-angle);
+
+        double sideways = x * Math.cos(-angle) - y * Math.sin(-angle);
+
+        sideways = sideways * 1.1;
+
+        double denom = Math.max(Math.abs(forward) + Math.abs(sideways) + Math.abs(turning),1);
+
+        // number
+        FR.setPower((forward - turning - sideways) / denom);
+        FL.setPower((forward + turning + sideways) / denom);
+        BR.setPower((forward - turning + sideways) / denom);
+        BL.setPower((forward + turning - sideways) / denom);
+
+//        FR.setPower((y - turning - x));
+//        FL.setPower((y + turning + x));
+//        BR.setPower((y - turning + x));
+//        BL.setPower((y + turning - x));
 
         telemetry.addData("caption", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
         telemetry.update();
 
-      
+
     }
 
 }
