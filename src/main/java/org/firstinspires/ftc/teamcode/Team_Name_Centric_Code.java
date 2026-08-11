0c41dacd4552124c7a2f903759438892ffaf955d
pymbotics <jzhang2028@pymblelc.nsw.edu.au>
2026-08-11 16:42:46 +1000

    hello

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Team_Name_Centric_Code.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp(group="Other Samples")
+public class Team_Name_Centric_Code extends OpMode
+{
+    // Declare Motors, Variables, and Functions
+    private DcMotor FRMotor;
+    private DcMotor FLMotor;
+    private DcMotor BRMotor;
+    private DcMotor BLMotor;
+    private IMU imu;
+
+    public int bob;
+
+    // storage location || Class || Name
+    // Private || DcMotor || BLMotor
+
+    // All Config and Hardware Mapping
+    @Override
+    public void init() {
+        // Hardware mapping tells the program what Variables corresponds to what motor in your Robot Configuration
+        FRMotor = hardwareMap.get(DcMotor.class, "fr");
+        FLMotor = hardwareMap.get(DcMotor.class, "fl");
+        BRMotor = hardwareMap.get(DcMotor.class, "br");
+        BLMotor = hardwareMap.get(DcMotor.class, "bl");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+// Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        imu.resetYaw();
+
+        // One side of motors will always need to be reversed so that they all spin in the same direction.
+        FRMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        FLMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        BRMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        BLMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // It is good practise to set the mode to ensure consistency
+        FRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BRMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BLMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+
+    @Override
+    public void loop() {
+        // Collect Necessary Data from Gamepad in This Loop
+        double ForwardBackwards = gamepad1.left_stick_y; // Y
+        double Turn = gamepad1.right_stick_x;
+        double LeftRight = gamepad1.left_stick_x; // X
+
+        double yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+
+
+
+        // For Speed Scaling
+        double SpeedScaler = 0.75;
+
+        // Example of Second Gamepad functioning in code
+        if(gamepad2.right_bumper) {
+            // Slow Button, if the Right Bumper on the second gamepad is being pressed, the robot will move slower
+            SpeedScaler = 0.25;
+        }
+
+
+        // Calculate the power for each Motor, combine the 3 above commented sections, multiply by the Scaler
+        double FRPower = (ForwardBackwards + Turn + LeftRight) * SpeedScaler;
+        double FLPower = (ForwardBackwards -Turn -LeftRight) * SpeedScaler;
+        double BRPower = (ForwardBackwards + Turn -LeftRight) * SpeedScaler;
+        double BLPower = (ForwardBackwards -Turn + LeftRight) * SpeedScaler;
+
+        // Set the power of the motors
+        FRMotor.setPower(FRPower);
+        FLMotor.setPower(FLPower);
+        BRMotor.setPower(BRPower);
+        BLMotor.setPower(BLPower);
+    }
+}
