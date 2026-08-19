582922e63a84b905f0134d66cef8504d4efd8b7e
Robotics-Team-6510 <RTeam@pymblelc.nsw.edu.au>
2026-08-19 18:27:56 +1000

    boo

--- src/main/java/org/firstinspires/ftc/teamcode/Shrek_Steak.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Shrek_Steak.java
@@ -18,6 +18,8 @@ public class Shrek_Steak extends OpMode
     private DcMotor BackLeftMotor;
     private DcMotor IntakeMotor;
     private IMU Gyro;
+    private boolean toggle;
+    private double speed_multiplier;
 
 
     /*
@@ -32,11 +34,14 @@ public class Shrek_Steak extends OpMode
         IntakeMotor = hardwareMap.get(DcMotor.class,  "intake");
         Gyro = hardwareMap.get(IMU.class,"imu");
 
-        FrontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        FrontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        BackRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        BackLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        IntakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        FrontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        FrontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        IntakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        toggle = false;
+        speed_multiplier = 0.75;
 
         RevHubOrientationOnRobot Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.RIGHT);
         IMU.Parameters Parameter = new IMU.Parameters(Orientation);
@@ -49,24 +54,33 @@ public class Shrek_Steak extends OpMode
 
     @Override
     public void loop() {
-        double Y_axis = gamepad1.right_stick_y;
-        double turn = -gamepad1.left_stick_x;
-        double X_axis = gamepad1.right_stick_x;
-        double speed_multiplier = 0.75;
-        boolean intake_button = gamepad1.a;
-        boolean outake_button = gamepad1.b;
+        double Y_axis = -gamepad1.left_stick_y;
+        double turn = -gamepad1.right_stick_x;
+        double X_axis = gamepad1.left_stick_x;
+        boolean intake_button = gamepad1.right_bumper;
+        boolean outake_button = gamepad1.left_bumper;
 
         double CurrentYaw = Gyro.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        double forward_and_back = X_axis*Math.sin(-CurrentYaw)+Y_axis*Math.cos(-CurrentYaw);
+
         double left_and_right = X_axis*Math.cos(-CurrentYaw)-Y_axis*Math.sin(-CurrentYaw);
+        double forward_and_back = X_axis*Math.sin(-CurrentYaw)+Y_axis*Math.cos(-CurrentYaw);
+
 
         left_and_right = left_and_right*1.1;
 
         double Normalise = Math.max(Math.abs(Y_axis)+Math.abs(X_axis)+Math.abs(turn),1);
 
-        if(gamepad1.right_bumper) {
-            speed_multiplier = 0.25;
+        if(gamepad1.aWasPressed()) {
+            toggle = !toggle;
         }
+
+        if(toggle) {
+            speed_multiplier = 0.25;
+        } else {
+            speed_multiplier = 0.75;
+        }
+
+
 
         if(gamepad1.options){
             Gyro.resetYaw();
@@ -80,10 +94,10 @@ public class Shrek_Steak extends OpMode
             IntakeMotor.setPower(0);
         }
 
-        double front_right_power = (forward_and_back + turn + left_and_right) / Normalise * speed_multiplier;
-        double front_left_power = (forward_and_back -turn -left_and_right) / Normalise * speed_multiplier;
-        double back_right_power = (forward_and_back + turn -left_and_right) / Normalise * speed_multiplier;
-        double back_left_power = (forward_and_back -turn + left_and_right) / Normalise * speed_multiplier;
+        double front_left_power = (forward_and_back + left_and_right + turn) / Normalise * speed_multiplier;
+        double back_left_power = (forward_and_back - left_and_right + turn) / Normalise * speed_multiplier;
+        double front_right_power = (forward_and_back - left_and_right - turn) / Normalise * speed_multiplier;
+        double back_right_power = (forward_and_back + left_and_right - turn) / Normalise * speed_multiplier;
 
         FrontRightMotor.setPower(front_right_power);
         FrontLeftMotor.setPower(front_left_power);
