93a41ec06c2a1c65737ef8f7e94a19db58a92cb1
Robotics-Team-6510 <RTeam@pymblelc.nsw.edu.au>
2026-08-26 16:15:46 +1000

    boo

--- src/main/java/org/firstinspires/ftc/teamcode/Challenge_Code.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Challenge_Code.java
@@ -11,8 +11,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp
 public class Challenge_Code extends OpMode {
     // section 1 - declaring variables - making variables
-    private DcMotor FrontRight, FrontLeft, BackLeft, BackRight, FirstFeeder;
+    private DcMotor FrontRight, FrontLeft, BackLeft, BackRight, FirstFeeder, SecondFeeder;
     private IMU imu;
+
+    double speed = 0.85;
 
     @Override
     public void init(){
@@ -22,6 +24,7 @@ public class Challenge_Code extends OpMode {
         BackLeft = hardwareMap.get(DcMotor.class, "BLW");
         BackRight = hardwareMap.get(DcMotor.class, "BRW");
         FirstFeeder = hardwareMap.get(DcMotor.class, "FF");
+        SecondFeeder = hardwareMap.get(DcMotor.class, "SF");
 
         FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -35,13 +38,12 @@ public class Challenge_Code extends OpMode {
 
     }
     @Override
-    public void loop(){
+    public void loop() {
         // actual code
 
         double forward = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
         double strafe = gamepad1.left_stick_x;
-        double speed = 0.85;
 
         if (gamepad1.left_bumper) {
             FirstFeeder.setPower(0.5);
@@ -51,10 +53,22 @@ public class Challenge_Code extends OpMode {
             FirstFeeder.setPower(0);
         }
 
-        if (gamepad1.left_trigger_pressed) {
+        if (gamepad1.a) {
+            SecondFeeder.setPower(0.35);
+        } else if (gamepad1.b) {
+            SecondFeeder.setPower(-0.35);
+        } else {
+            SecondFeeder.setPower(0);
+        }
+
+        if (gamepad1.leftTriggerWasPressed()) {
+            speed += 0.1;
+            if (speed > 1) speed = 1;
+        } else if (gamepad1.rightTriggerWasPressed()) {
+            speed -= 0.1;
+            if (speed < 0.1) speed = 0.1;
+        } else if (gamepad1.y) {
             speed = 0.85;
-        } else if (gamepad1.right_trigger_pressed) {
-            speed = 0.3;
         }
 
         if (gamepad1.options) {
