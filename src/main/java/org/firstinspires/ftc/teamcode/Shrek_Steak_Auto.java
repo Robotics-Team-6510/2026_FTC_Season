aa51c5bd8c9d60c0bf8df89ba5cd042b76fd1068
Asaphfirst <32269235+Asaphfirst@users.noreply.github.com>
2026-09-02 17:02:27 +1000

    Auto

--- src/main/java/org/firstinspires/ftc/teamcode/Shrek_Steak_Auto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Shrek_Steak_Auto.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
@@ -19,7 +20,7 @@ public class Shrek_Steak_Auto extends LinearOpMode {
     private DcMotor BackRightMotor;
     private DcMotor BackLeftMotor;
     private DcMotor IntakeMotor;
-//    private DcMotor FeederMotor;
+    //    private DcMotor FeederMotor;
     private IMU Gyro;
 
 
@@ -30,9 +31,9 @@ public class Shrek_Steak_Auto extends LinearOpMode {
         FrontLeftMotor = hardwareMap.get(DcMotor.class, "fl");
         BackRightMotor = hardwareMap.get(DcMotor.class, "br");
         BackLeftMotor = hardwareMap.get(DcMotor.class, "bl");
-        IntakeMotor = hardwareMap.get(DcMotor.class,  "intake");
-//        FeederMotor = hardwareMap.get(DcMotor.class, "feeder");
-        Gyro = hardwareMap.get(IMU.class,"imu");
+        IntakeMotor = hardwareMap.get(DcMotor.class, "intake");
+        // FeederMotor = hardwareMap.get(DcMotor.class, "feeder");
+        Gyro = hardwareMap.get(IMU.class, "imu");
 
         FrontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         FrontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -48,29 +49,62 @@ public class Shrek_Steak_Auto extends LinearOpMode {
         waitForStart();
         // Auto Sequence
 
+        forward(1000, 1);
+        forward(-1000, 1);
+        turn(1000, 1);
+
+//        IntakeMotor.setPower(0.75);
+//        sleep(2000);
+//        IntakeMotor.setPower(-0.75);
+//        sleep(5000);
+//        IntakeMotor.setPower(0);
+    }
+
+    void forward(int distance, double power) {
         FrontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         FrontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BackLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BackRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        FrontLeftMotor.setTargetPosition(1000);
-        FrontRightMotor.setTargetPosition(1000);
-        BackLeftMotor.setTargetPosition(1000);
-        BackRightMotor.setTargetPosition(1000);
+        FrontLeftMotor.setTargetPosition(distance);
+        FrontRightMotor.setTargetPosition(distance);
+        BackLeftMotor.setTargetPosition(distance);
+        BackRightMotor.setTargetPosition(distance);
 
-        FrontLeftMotor.setPower(1);
-        FrontRightMotor.setPower(1);
-        BackLeftMotor.setPower(1);
-        BackRightMotor.setPower(1);
+        FrontLeftMotor.setPower(power);
+        FrontRightMotor.setPower(power);
+        BackLeftMotor.setPower(power);
+        BackRightMotor.setPower(power);
 
         FrontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FrontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BackLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BackRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        while (FrontLeftMotor.isBusy()) {
+        }
+    }
 
+    void turn(int distance, double power) {
+        FrontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        FrontLeftMotor.setTargetPosition(distance);
+        FrontRightMotor.setTargetPosition(-distance);
+        BackLeftMotor.setTargetPosition(distance);
+        BackRightMotor.setTargetPosition(-distance);
 
+        FrontLeftMotor.setPower(power);
+        FrontRightMotor.setPower(power);
+        BackLeftMotor.setPower(power);
+        BackRightMotor.setPower(power);
+
+        FrontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 }
