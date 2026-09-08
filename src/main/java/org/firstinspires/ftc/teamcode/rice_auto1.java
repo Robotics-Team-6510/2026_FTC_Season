b988a38101b5668286057a24f7ea787b096d4f34
RTeam <rteam@pymblelc.nsw.edu.au>
2026-09-08 18:15:33 +1000

    hi

--- src/main/java/org/firstinspires/ftc/teamcode/rice_auto1.java
+++ src/main/java/org/firstinspires/ftc/teamcode/rice_auto1.java
@@ -3,10 +3,9 @@ package org.firstinspires.ftc.teamcode;
 
 // Prewritten Code that is imported so we don't have to write EVERYTHING from scratch.
 // Delete what you don't need.
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -14,7 +13,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 // Do NOT give it a name "name = "something"", leave the name blank, and it will use the filename
 // Code with the same group name will be grouped together in the driver station
 @Autonomous
-public class Rice_Auto extends LinearOpMode {
+public class rice_auto1 extends LinearOpMode {
     // Declare OpMode members, put motors, devices, etc all here. i.e: private DcMotor LeftFront;
     // 1
     DcMotor lf_motor, lb_motor, rf_motor, rb_motor;
